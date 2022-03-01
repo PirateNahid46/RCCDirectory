797f1fc9581931d4aeb0ff133701ac268dbbe6fe
PirateNahid46 <98163985+PirateNahid46@users.noreply.github.com>
2022-03-01 13:32:01 +0600

    commit

--- app/src/main/java/com/rcc/rccdirectory/DetailsActivity.java
+++ app/src/main/java/com/rcc/rccdirectory/DetailsActivity.java
@@ -20,10 +20,11 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class DetailsActivity extends AppCompatActivity {
-    TextView cadetNo,nameView;
+    TextView cadetNo,nameView, batchView, houseView, addressView, mobileView, workView, emailView;
     ImageView imageView;
     DatabaseReference reference;
     StorageReference storageReference;
+    String name, cn , batch, house, home, district, mobile, work, email;
 
 
 
@@ -32,14 +33,38 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         reference = FirebaseDatabase.getInstance().getReference("list");
-        String cn = getIntent().getStringExtra("cn");
+        cn = getIntent().getStringExtra("cn");
         storageReference = FirebaseStorage.getInstance().getReference("profile.png");
-        String name = getIntent().getStringExtra("name");
+        name = getIntent().getStringExtra("name");
+        batch = getIntent().getStringExtra("batch");
+        house = getIntent().getStringExtra("house");
+        home = getIntent().getStringExtra("home");
+        district = getIntent().getStringExtra("district");
+        mobile = getIntent().getStringExtra("mobile");
+        work = getIntent().getStringExtra("work");
+        email = getIntent().getStringExtra("email");
+
+
+
         cadetNo = findViewById(R.id.cadetNo);
         nameView = findViewById(R.id.nameView);
         imageView = findViewById(R.id.profileImage);
+        batchView = findViewById(R.id.batchView);
+        houseView = findViewById(R.id.houseView);
+        addressView = findViewById(R.id.addressV);
+        mobileView = findViewById(R.id.mobileV);
+        workView = findViewById(R.id.workV);
+        emailView = findViewById(R.id.emailV);
+
+
         cadetNo.setText("Cadet no: " + cn);
         nameView.setText("Name: " + name);
+        batchView.setText("Batch: " + batch);
+        houseView.setText("House: "+ house);
+        addressView.setText("Address: " + home + ", "+ district);
+        mobileView.setText("Mobile: "+ mobile);
+        workView.setText("Work: "+ work);
+        emailView.setText("Email: "+ email);
 
         final long ONE_MEGABYTE = 1024 * 1024;
         storageReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(bytesPrm -> {
--- app/src/main/java/com/rcc/rccdirectory/MainActivity.java
+++ app/src/main/java/com/rcc/rccdirectory/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase database;
     DatabaseReference reference;
-    ArrayList<String> list , cadetNo, nameList;
+    ArrayList<String> list , cadetNo, nameList, batchList, houseList, homeList, disList, conList, workList, emailList;
     ArrayAdapter<String> adapter;
 
 
@@ -39,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
         list = new ArrayList<String>();
         cadetNo = new ArrayList<String>();
         nameList = new ArrayList<String>();
+        batchList = new ArrayList<String>();
+        houseList = new ArrayList<String>();
+        homeList = new ArrayList<String>();
+        disList = new ArrayList<String>();
+        conList = new ArrayList<String>();
+        workList = new ArrayList<String>();
+        emailList = new ArrayList<String>();
         adapter = new ArrayAdapter<String>(this, R.layout.item, R.id.itemTextView, list );
 
 
@@ -49,9 +56,28 @@ public class MainActivity extends AppCompatActivity {
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 String name = dataSnapshot.child("name").getValue(String.class);
                 String cn = dataSnapshot.child("cn").getValue(String.class);
+                String batch = dataSnapshot.child("batch").getValue(String.class);
+                String contact = dataSnapshot.child("contact").getValue(String.class);
+                String district = dataSnapshot.child("district").getValue(String.class);
+                String home = dataSnapshot.child("home").getValue(String.class);
+                String house = dataSnapshot.child("house").getValue(String.class);
+                String work = dataSnapshot.child("work").getValue(String.class);
+                String email = dataSnapshot.child("email").getValue(String.class);
+
+
+
+
                 list.add(name +"\n"+ cn);
                 nameList.add(name);
                 cadetNo.add(cn);
+                batchList.add(batch);
+                houseList.add(house);
+                homeList.add(home);
+                disList.add(district);
+                conList.add(contact);
+                workList.add(work);
+                emailList.add(email);
+
                 adapter.notifyDataSetChanged();
 
 
@@ -81,11 +107,17 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String Cn = cadetNo.get(i);
-                String name = nameList.get(i);
                 Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
-                intent.putExtra("cn", Cn);
-                intent.putExtra("name", name);
+                intent.putExtra("cn", cadetNo.get(i));
+                intent.putExtra("name", nameList.get(i));
+                intent.putExtra("batch", batchList.get(i));
+                intent.putExtra("house",houseList.get(i));
+                intent.putExtra("home", homeList.get(i));
+                intent.putExtra("district", disList.get(i));
+                intent.putExtra("mobile", conList.get(i));
+                intent.putExtra("work", workList.get(i));
+                intent.putExtra("email", emailList.get(i));
+
                 startActivity(intent);
 
             }
